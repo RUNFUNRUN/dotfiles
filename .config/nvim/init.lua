33c4c2816e0ed8dae4c903d763e9b6cb4d7eec9e
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-11-10 19:53:18 +0900

    update

---
+++ .config/nvim/init.lua
@@ -0,0 +1,30 @@
+-- for VSCode Neovim plugin
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({ 'machakann/vim-sandwich' })
+
+vim.opt.clipboard = 'unnamedplus'
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set('n', '<Esc><Esc>', '<Cmd>nohlsearch<CR>', opts)
+vim.keymap.set('i', 'jj', '<Esc>', opts)
+vim.keymap.set('n', 'H', '<Cmd>bprevious<CR>', opts)
+vim.keymap.set('n', 'L', '<Cmd>bnext<CR>', opts)
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', 's', '<Nop>', opts)
