55d77d96f202f4cace8937904aee06ce81016ae6
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-11-10 22:06:33 +0900

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -28,3 +28,7 @@ vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
 vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
 vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
 vim.keymap.set('n', 's', '<Nop>', opts)
+
+if vim.g.vscode then
+  vim.keymap.set('i', '<Right>', "<Cmd>call VSCodeCall('editor.action.inlineSuggest.commit')<CR>", opts)
+end
