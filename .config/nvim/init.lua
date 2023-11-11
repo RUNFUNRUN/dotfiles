df82f0df1a7f6afb960cc87fc03fcab7c8c52078
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-11-11 20:51:48 +0900

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,3 +21,17 @@ local opts = { noremap = true, silent = true }
 
 vim.keymap.set('n', '<Esc><Esc>', '<Cmd>nohlsearch<CR>', opts)
 vim.keymap.set('n', 's', '<Nop>', opts)
+
+-- only wsl
+vim.g.clipboard = {
+  name = 'myClipboard',
+  copy = {
+    ['+'] = 'win32yank.exe -i --crlf',
+    ['*'] = 'win32yank.exe -i --crlf'
+  },
+  paste = {
+    ['+'] = 'win32yank.exe -o --lf',
+    ['*'] = 'win32yank.exe -o --lf'
+  },
+  cache_enabled = 1
+}
