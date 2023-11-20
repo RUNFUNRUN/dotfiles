cb69bd095adc9679a03bfd07e8cb848596daf929
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-11-20 14:14:27 +0900

    update

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -19,6 +19,11 @@ lvim.plugins = {
     cmd = { 'GistCreate', 'GistCreateFromFile', 'GistsList' },
     config = true
   },
+  {
+    'samjwill/nvim-unception',
+    lazy = false,
+    init = function() vim.g.unception_block_while_host_edits = true end
+  },
 }
 
 require('code_runner').setup({
