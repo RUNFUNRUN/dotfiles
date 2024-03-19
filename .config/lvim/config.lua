b89906379e58a4fb525f2748c4e40746bdb6ead7
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-03-20 02:42:54 +0900

    update

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -139,7 +139,7 @@ require('tokyonight').setup({
   }
 })
 
--- dashboard settings
+-- lvin builtin settings
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = 'dashboard'
 lvim.builtin.alpha.dashboard.section.header.val = {
@@ -153,8 +153,6 @@ lvim.builtin.alpha.dashboard.section.header.val = {
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = 'left'
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-
--- Automatically install missing parsers when entering buffer
 lvim.builtin.treesitter.auto_install = true
 
 -- apply mdx filetype
