7cf59b772226795c22cdac295bd9a209c33edbbf
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-03-21 09:20:03 +0900

    update

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -321,15 +321,15 @@ if is_eslint_config_present() then
 end
 
 -- only wsl
-vim.g.clipboard = {
-  name = 'myClipboard',
-  copy = {
-    ['+'] = 'win32yank.exe -i --crlf',
-    ['*'] = 'win32yank.exe -i --crlf'
-  },
-  paste = {
-    ['+'] = 'win32yank.exe -o --lf',
-    ['*'] = 'win32yank.exe -o --lf'
-  },
-  cache_enabled = 1
-}
+-- vim.g.clipboard = {
+--   name = 'myClipboard',
+--   copy = {
+--     ['+'] = 'win32yank.exe -i --crlf',
+--     ['*'] = 'win32yank.exe -i --crlf'
+--   },
+--   paste = {
+--     ['+'] = 'win32yank.exe -o --lf',
+--     ['*'] = 'win32yank.exe -o --lf'
+--   },
+--   cache_enabled = 1
+-- }
